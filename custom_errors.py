dd9042f84c8fdcc14a0f24e34a25f7d4d271c94b
Andrew S <andrew.senkowski@zingermans.com>
2026-02-04 15:36:03 -0500

    aggregate_sales script connects to Squirrel and gets the right data

--- custom_errors.py
+++ custom_errors.py
@@ -1,5 +1,8 @@
 class InvalidSquirrelQuery(LookupError):
     ''' For any queries that result in no results '''
     def __init__(self, message, start_time, end_time):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
+        self.start_time = start_time
+        self.end_time = end_time
+    def __str__(self):
+        return f"{self.message} for query {self.start_time}-{self.end_time}"
